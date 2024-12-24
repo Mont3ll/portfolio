b7f7b97fba0b53c6a415713eaa48dda3458a5115
Montell Luseno <meluseno@gmail.com>
2024-12-24 16:03:43 +0300

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import Intro from '@/components/intro'
+import NewsletterForm from '@/components/newsletter-form'
+import RecentPosts from '@/components/recent-posts'
+import RecentProjects from '@/components/recent-projects'
+
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container max-w-3xl'>
-        <h1 className='text-3xl font-bold'>Portfolio</h1>
+        <Intro />
+
+        <RecentPosts />
+        <RecentProjects />
+
+        <NewsletterForm />
       </div>
     </section>
   )
