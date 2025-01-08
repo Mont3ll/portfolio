fa917f92af5fa5405a3ed7635ba9c761eabf1225
mont3ll <meluseno@gmail.com>
2025-01-08 10:45:29 +0300

    edit intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,10 +6,11 @@ export default function Intro() {
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
         <h1 className='title no-underline'>Hey, I&#39;m Montell.</h1>
-        <p className='text-muted-foreground mt-3 font-light'>
-          I&#39;m a software engineer based in Nairobi, Kenya. I&#39;m
-          passionate about learning new technologies and sharing knowledge with
-          others.
+        <p className='mt-3 font-light text-muted-foreground'>
+          I&#39;m a professional web developer with a knack for crafting
+          visually stunning and highly functional websites. Combining creativity
+          and technical expertise. I transform your vision into digital
+          masterpiece that excels in both appearance and performance.
         </p>
       </div>
       <div className='relative'>
